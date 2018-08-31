50722c7caae206a79b2af2859bd1a2cbdc2faddf
mkctkr <mkctkr@gmail.com>
2018-08-31 16:13:39 +0900

    Update MatchingCenter.sol

--- MatchingCenter.sol
+++ MatchingCenter.sol
@@ -1,74 +1,103 @@
 pragma solidity ^0.4.24;
-
+ 
 contract MatchingCenter{ 
+    // ======================================================================================
+    // Donator-Patient matching states
+    // ======================================================================================
+    //enum BLOOD_TYPE { O, A, B, AB }
+    enum STATE { CREATED, REGISTER_OPENED, REGISTER_CLOSED, ANNOUNCED }
+    STATE public state;
+    address public governor;
+    
+    modifier requiredState(STATE st) { require(st == state, 'Invalid State'); _; }
+    modifier onlyGovernor() { require(msg.sender == governor, 'msg.sender is not Governor'); _; }
+ 
     struct Donator {
         string  Name;
         string  BloodType;
         address Addr;
         address patientAddress;
     }
-
+ 
     struct Patient {
         string  Name;
         string  BloodType;
         address Addr;
     }
-
+ 
     struct MatchResult{
         address patientAddress;
         address donatorAddress;
     }
-
+    
     address owner;
+ 
     address[] public patientList;
     address[] public donatorList;
     MatchResult[] public matchedList;
-
+ 
     mapping ( address => Patient )     patients;
     mapping ( address => Donator )     donators;
-
+    
+    constructor() public {
+        state = STATE.CREATED;
+        governor = msg.sender;
+    }
+    
     function Strcmp (string a, string b) view returns (bool){
        return keccak256(a) == keccak256(b);
     }
-
-    function registerDonator(string _name, string _bloodtype, address _patientAddr) public requiredState(STATE.REGISTER_OPENED) {
+    
+    function openRegistration() public onlyGovernor requiredState(STATE.CREATED) {
+        state = STATE.REGISTER_OPENED;
+    }
+    
+    function closeRegistartion() public onlyGovernor requiredState(STATE.REGISTER_OPENED) {
+        state = STATE.REGISTER_CLOSED;
+    }
+ 
+    function registerDonator(string _name, string _bloodtype, address _patientAddr) public{
         donators[msg.sender].Name = _name;
         donators[msg.sender].BloodType = _bloodtype;
         donators[msg.sender].patientAddress = _patientAddr;
+ 
         donatorList.push( msg.sender );
     } 
-
-    function registerPatient(string _name, string _bloodtype) public  requiredState(STATE.REGISTER_OPENED) {
+ 
+    function registerPatient(string _name, string _bloodtype) public{
         patients[msg.sender].Name = _name;
         patients[msg.sender].BloodType = _bloodtype;
+ 
         patientList.push( msg.sender );
     }
-    
+ 
     function getPatient(address _addr) public returns (string _name, string _bloodtype) {
         return (patients[_addr].Name, patients[_addr].BloodType);
     }
-
+    
     function getDonator(address _addr) public returns (string _name, string _bloodtype, address _patientaddr) {
         return (donators[_addr].Name, donators[_addr].BloodType, donators[_addr].patientAddress);
     }
-
+ 
     // two step 
     // 1. Donator -> Patient 
     // 2. Patient -> Donator
-    function DeterMatching() public onlyGovernor {
+ 
+    function Announce_DeterMatching() public onlyGovernor {
+        
         // first step : Donator -> Patient 
         for(uint i=0; i<donatorList.length; i++)
         {
             for(uint j=0; j<patientList.length; j++)
             {
-                if(donators[donatorList[i]].patientAddress == patientList[j])
+                if(donators[donatorList[i]].patientAddress == patientList[j] && donableChk(patientList[i], donatorList[j]) != 0)
                 {
                     regMatchset(patientList[j], donatorList[i]);
                     break;
                 }
             }
         }
-
+        
         // second step : Patient -> Donator
         for(i=0; i<patientList.length; i++)
         {
@@ -87,37 +116,42 @@ contract MatchingCenter{
         
         state = STATE.ANNOUNCED;
     }
-
+ 
     /*
     function getMatchedList() public return (MatchResult[]) {
         return matchedList[];
     }
     */
-
-    function regMatchset (address pat, address don) private onlyGovernor {
+ 
+    function regMatchset(address pat, address don) public onlyGovernor {
         MatchResult storage matchresult;
-
+        
         matchresult.patientAddress = pat;
         matchresult.donatorAddress = don;
-
+ 
         matchedList.push( matchresult );
     }
-
-    function donableChk(address pat, address don) private view returns (uint){
+ 
+    function donableChk(address pat, address don) public onlyGovernor returns (uint){
         string patBloodTp = patients[pat].BloodType;
         string donBloodTp = donators[don].BloodType;
-        uint prior = 0;
-
-        prior = ( Strcmp(patBloodTp, donBloodTp) ) ? 1 : ( !Strcmp(patBloodTp, donBloodTp) && Strcmp(donBloodTp, "O") ? 2 : 0 );
-
-        return prior;
-
+        
+        // Full Searching matchedList
+        for(uint i=0; i<matchedList.length; i++)
+        {
+            if( pat == matchedList[i].patientAddress || don == matchedList[i].donatorAddress )
+            {
+                return 0;
+            }
+        }
+        
+        return ( Strcmp(patBloodTp, donBloodTp) ) ? 1 : ( !Strcmp(patBloodTp, donBloodTp) && Strcmp(donBloodTp, "O") ? 2 : 0 );
+ 
         /*
         if(patBloodTp == donBloodTp)
         {
             prior = 1;
         }
-
         else if(patBloodTp != donBloodTp && donBloodTp == "O")
         {
             prior = 2;
@@ -126,38 +160,11 @@ contract MatchingCenter{
         {
             prior = 0;
         }
-
-        return prior;
         */
     }
-
-    constructor() public {
-        state = STATE.CREATED;
-        governor = msg.sender;
-    }
-
+ 
     // ======================================================================================
-    // Donator-Patient matching states
-    // ======================================================================================
-    enum STATE { CREATED, REGISTER_OPENED, REGISTER_CLOSED, ANNOUNCED }
-    STATE public state;
-    address public governor;
-
-    modifier requiredState(STATE st) { require(st == state, 'Invalid State'); _; }
-    modifier onlyGovernor() { require(msg.sender == governor, 'msg.sender is not Governor'); _; }
-
-    function openRegistration() public onlyGovernor requiredState(STATE.CREATED) {
-        state = STATE.REGISTER_OPENED;
-    }
-
-    function closeRegistartion() public onlyGovernor requiredState(STATE.REGISTER_OPENED) {
-        state = STATE.REGISTER_CLOSED;
-        
-        DeterMatching();
-    }
-    // ======================================================================================
-
-
+    
     // ======================================================================================
     // Browsing match result APIs
     // ======================================================================================
@@ -185,15 +192,15 @@ contract MatchingCenter{
                 }
             }
         }
-
+        
         return (false, "", "");
     }
-
+    
     function getNumberOfMatchedPair() public view requiredState(STATE.ANNOUNCED) onlyGovernor returns (uint)
     {
         return matchedList.length;
     }
-
+    
     // return: (Donator name, Donator BloodType, Patient name, Patient BloodType)
     function browseMatchedList(uint index) public view requiredState(STATE.ANNOUNCED) onlyGovernor returns (string, string, string, string)
     {
@@ -201,10 +208,10 @@ contract MatchingCenter{
         {
             revert("Index out of bound");
         }
-
+        
         Donator storage d = donators[matchedList[index].donatorAddress];
         Patient storage p = patients[matchedList[index].patientAddress];
-
+        
         return (d.Name, d.BloodType, p.Name, p.BloodType);
     }
     // ======================================================================================
